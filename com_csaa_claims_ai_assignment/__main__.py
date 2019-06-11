d94e20d80f48126e63b5034844085204ff4670e4
Husain <epxvhus@tent.trt.csaa.pri>
2019-06-11 01:26:00 +0100

    adding waitress

--- com_csaa_claims_ai_assignment/__main__.py
+++ com_csaa_claims_ai_assignment/__main__.py
@@ -3,16 +3,18 @@
 import connexion
 
 from com_csaa_claims_ai_assignment import encoder
+from waitress import serve 
 
 
 def application():
     #app = connexion.App(__name__, specification_dir='./openapi/')
-    app = connexion.App(__name__, specification_dir='./openapi/', server='gevent')
+    app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml',
                 arguments={'title': 'AiClaimAssignmentAPI'},
                 pythonic_params=True)
-    app.run(port=8080)
+    #app.run(port=8080)
+    serve(app, host='0.0.0.0', port=8080) 
 
 
 if __name__ == '__main__':
